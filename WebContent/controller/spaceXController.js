5d9304b0f976f869597949241852b07fbc2d4eb3
rupali100 <vrupali100@gmail.com>
2020-08-23 00:54:54 +0530

    update

--- WebContent/controller/spaceXController.js
+++ WebContent/controller/spaceXController.js
@@ -2,9 +2,14 @@
 "use strict";
 var xmlhttp = new XMLHttpRequest();
 var url = "https://api.spacexdata.com/v3/launches?";
-
+var myArr;
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    var myArr = JSON.parse(this.responseText);
+    myArr = JSON.parse(this.responseText);
   }
 };
+
+loadSpaceXComponent = function(){
+	
+	
+}
