a351755dc417deae3a07d592e4741eedae3adfaa
rupali100 <vrupali100@gmail.com>
2020-08-22 20:37:18 +0530

    update

---
+++ WebContent/controller/spaceXController.js
@@ -0,0 +1,11 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "https://api.spacexdata.com/v3/launches?limit=100";
+
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myArr = JSON.parse(this.responseText);
+    myFunction(myArr);
+  }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
