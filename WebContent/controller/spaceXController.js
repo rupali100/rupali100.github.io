c0b59b53bb72a09a3af95559976e66ced661b324
rupali100 <vrupali100@gmail.com>
2020-08-23 05:15:05 +0530

    update

--- WebContent/controller/spaceXController.js
+++ WebContent/controller/spaceXController.js
@@ -1,21 +1,28 @@
 var xmlhttp = new XMLHttpRequest();
 var url = "https://api.spaceXdata.com/v3/launches?";
-
+let spaceXdata;
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    var myArr = JSON.parse(this.responseText);
-    myFunction(myArr);
+     spaceXdata = JSON.parse(this.responseText);
+    loadSpaceXData(spaceXdata);
   }
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-function myFunction(arr) {
-  var out = "";
-  var i;
-  for(i = 0; i < arr.length; i++) {
-    out += '<a href="' + arr[i].url + '">' + 
-    arr[i].display + '</a><br>';
-  }
-  document.getElementById("id01").innerHTML = out;
+function loadSpaceXData(spaceXdata) {
+	let arrLength = arr.length;
+	let i=0;
+	let aSpaceX = [];
+	for(i; i<=arrLength-1; i++){
+
+	let oSpaceX = {
+						mission_name: arr[i].mission_name,
+						mission_id:  arr[i].mission_id,
+						launch_year:  arr[i].launch_year,
+						succ_launch:  arr[i].launch_success,
+						succ_landing: arr[i].rocket.first_stage.cores[0].landing_intent,
+					}
+					aSpaceX.push(oSpaceX);
+	}
 }
