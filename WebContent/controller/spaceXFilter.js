714ec2c878d261390c60e178ae5e9d00376b2110
rupali100 <vrupali100@gmail.com>
2020-08-23 12:00:57 +0530

    fsa

--- WebContent/controller/spaceXFilter.js
+++ WebContent/controller/spaceXFilter.js
@@ -1,6 +1,16 @@
 function onClickYear(data){
-	let filterVal = event.target.id;
+	let filterVal = event.target.innerText;
 	return "dd";
-	
+	var xmlhttp = new XMLHttpRequest();
+	var url = "https://api.spaceXdata.com/v3/launches?";
+	let spaceXdata;
+	xmlhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+	     spaceXdata = JSON.parse(this.responseText);
+	    loadSpaceXData(spaceXdata);
+	  }
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
 	
 }
