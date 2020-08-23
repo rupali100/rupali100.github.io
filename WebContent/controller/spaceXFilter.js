0cc4e3c4166a2e1dd376376350365b305616ce10
rupali100 <vrupali100@gmail.com>
2020-08-23 20:25:11 +0530

    gg

--- WebContent/controller/spaceXFilter.js
+++ WebContent/controller/spaceXFilter.js
@@ -1,24 +1,9 @@
 function onClickYear(data){
-	this.prevYear = event.target.innerText;
-	let filterVal = event.target.innerText; 
-	var xmlhttp = new XMLHttpRequest();
-	if(this.prevLaunch && this.prevLand ){
-		var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_success="+this.prevLaunch.toLowerCase()+"&land_success="+this.prevLand.toLowerCase()+"&launch_year="+filterVal;
-	}else if(this.prevLaunch || this.prevLand){
-		if(this.prevLaunch){
-			var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_success="+this.prevLaunch.toLowerCase()+"&launch_year="+filterVal;
-			
-		}else{
-			var url = "https://api.spaceXdata.com/v3/launches?limit=100&land_success="+this.prevLand.toLowerCase()+"&launch_year="+filterVal;
-				
-		}
-		
-	}	
-	else{
-		var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_year="+filterVal ;
-	}
-	document.location.search=url.split("?")[1]
 	
+	
+	let filterVal = event.target.innerText; 	
+	var xmlhttp = new XMLHttpRequest();
+	var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_year="+filterVal ;
 	let spaceXdata;
 	xmlhttp.onreadystatechange = function() {
 	  if (this.readyState == 4 && this.status == 200) {
@@ -31,25 +16,9 @@ function onClickYear(data){
 };
 
 function onClickLaunch(){
-	this.prevLaunch= event.target.innerText; 
-	let filterVal = event.target.innerText;
+	let filterVal = event.target.innerText; 	
 	var xmlhttp = new XMLHttpRequest();
-	if(this.prevYear && this.prevLand ){
-		var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_year="+this.prevYear+"&land_success="+this.prevLand+"&launch_success="+filterVal.toLowerCase();
-	}else if(this.prevLaunch || this.prevLand){
-		if(this.prevYear){
-			var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_year="+this.prevYear+"&launch_success="+filterVal.toLowerCase();
-			
-		}else{
-			var url = "https://api.spaceXdata.com/v3/launches?limit=100&land_success="+this.prevLand.toLowerCase()+"&launch_success="+filterVal.toLowerCase();
-				
-		}
-		
-	}	
-	else{
-		var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_success="+filterVal.toLowerCase() ;
-	}
-	document.location.search=url.split("?")[1]
+	var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_success="+filterVal ;
 	let spaceXdata;
 	xmlhttp.onreadystatechange = function() {
 	  if (this.readyState == 4 && this.status == 200) {
@@ -63,26 +32,9 @@ function onClickLaunch(){
 };
 
 function onClickLand(){
-	this.prevLand= event.target.innerText; 
 	let filterVal = event.target.innerText; 	
 	var xmlhttp = new XMLHttpRequest();
-	if(this.prevYear && this.prevLaunch ){
-		var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_year="+this.prevYear+"&launch_success="+this.prevLaunch.toLowerCase()+"&land_success="+filterVal.toLowerCase();
-	}else if(this.prevLaunch || this.prevYear){
-		if(this.prevYear){
-			var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_year="+this.prevYear+"&land_success="+filterVal.toLowerCase();
-			
-		}else{
-			var url = "https://api.spaceXdata.com/v3/launches?limit=100&land_success="+this.prevLaunch.toLowerCase()+"&land_success="+filterVal.toLowerCase();
-				
-		}
-		
-	}	
-	else{
-		var url = "https://api.spaceXdata.com/v3/launches?limit=100&land_success="+filterVal.toLowerCase() ;
-	}
-	
-	document.location.search=url.split("?")[1]
+	var url = "https://api.spaceXdata.com/v3/launches?limit=100&land_success="+filterVal ;
 	let spaceXdata;
 	xmlhttp.onreadystatechange = function() {
 	  if (this.readyState == 4 && this.status == 200) {
