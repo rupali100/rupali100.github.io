2afb62d32e45a90110a1c0d7837d7beb1737b4f6
rupali100 <vrupali100@gmail.com>
2020-08-23 12:07:41 +0530

    update

--- WebContent/controller/spaceXController.js
+++ WebContent/controller/spaceXController.js
@@ -1,5 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "https://api.spaceXdata.com/v3/launches?";
+var url = "https://api.spaceXdata.com/v3/launches?limit=100";
 let spaceXdata;
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
--- WebContent/controller/spaceXFilter.js
+++ WebContent/controller/spaceXFilter.js
@@ -2,7 +2,7 @@ function onClickYear(data){
 	let filterVal = event.target.innerText;
 	return "dd";
 	var xmlhttp = new XMLHttpRequest();
-	var url = "https://api.spaceXdata.com/v3/launches?";
+	var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_year="+filterVal ;
 	let spaceXdata;
 	xmlhttp.onreadystatechange = function() {
 	  if (this.readyState == 4 && this.status == 200) {
@@ -12,5 +12,10 @@ function onClickYear(data){
 	};
 	xmlhttp.open("GET", url, true);
 	xmlhttp.send();
+	
+}
+
+function onClickLaunch(){
+	return "xxx"
 	
 }
