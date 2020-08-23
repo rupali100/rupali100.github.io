9b9b4b51620af6992237ddce838018c0e256d251
rupali100 <vrupali100@gmail.com>
2020-08-23 13:05:34 +0530

    update

--- WebContent/controller/spaceXFilter.js
+++ WebContent/controller/spaceXFilter.js
@@ -1,5 +1,5 @@
 function onClickYear(data){
-	let filterVal = event.target.innerText;
+	let filterVal = event.target.innerText; 	
 	var xmlhttp = new XMLHttpRequest();
 	var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_year="+filterVal ;
 	let spaceXdata;
@@ -11,10 +11,36 @@ function onClickYear(data){
 	};
 	xmlhttp.open("GET", url, true);
 	xmlhttp.send();
-	
-}
+};
 
 function onClickLaunch(){
-	return "xxx"
+	let filterVal = event.target.innerText; 	
+	var xmlhttp = new XMLHttpRequest();
+	var url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_success="+filterVal ;
+	let spaceXdata;
+	xmlhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+	     spaceXdata = JSON.parse(this.responseText);
+	    loadSpaceXData(spaceXdata);
+	  }
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
 	
+};
+
+function onClickLand(){
+	let filterVal = event.target.innerText; 	
+	var xmlhttp = new XMLHttpRequest();
+	var url = "https://api.spaceXdata.com/v3/launches?limit=100&land_success="+filterVal ;
+	let spaceXdata;
+	xmlhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+	     spaceXdata = JSON.parse(this.responseText);
+	    loadSpaceXData(spaceXdata);
+	  }
+	};
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
 }
+
