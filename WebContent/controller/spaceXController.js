c32a2f9b920ece12c5eb53376c4b49ce14094238
rupali100 <vrupali100@gmail.com>
2020-08-23 07:02:12 +0530

    update

--- WebContent/controller/spaceXController.js
+++ WebContent/controller/spaceXController.js
@@ -10,13 +10,14 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-function loadSpaceXData(spaceXdata) {
+function loadSpaceXData(arr) {
 	let arrLength = arr.length;
 	let i=0;
 	let aSpaceX = [];
 	for(i; i<=arrLength-1; i++){
 
 	let oSpaceX = {
+						space_image: arr[0].links.mission_patch_small,
 						mission_name: arr[i].mission_name,
 						mission_id:  arr[i].mission_id,
 						launch_year:  arr[i].launch_year,
@@ -25,4 +26,32 @@ function loadSpaceXData(spaceXdata) {
 					}
 					aSpaceX.push(oSpaceX);
 	}
+	
+	let element = document.createElement("div")l
+	element.appendChild(document.createElement("div"));
+	element.childNodes[0].setAttribute("class","cell")
+	element.childNodes[0].appendChild(document.createElement("img"));
+	element.childNodes[0].childNodes[0].setAttribute("src",aSpaceX[i].space_image);
+	element.childNodes[0].childNodes[0].setAttribute("alt","image");
+	element.childNodes[0].childNodes[0].setAttribute("class","abc");
+	
+	element.appendChild(document.createElement("div"))
+	element.childNodes[1].setAttribute("class","cell")
+	element.childNodes[1].appendChild(document.createElement("strong"));
+	element.childNodes[1].childNodes[0].innerText = aSpaceX[i].mission_name;
+		
+	element.appendChild(document.createElement("div"))
+	element.childNodes[2].setAttribute("class","cell")
+	element.childNodes[2].appendChild(document.createElement("span"));
+	element.childNodes[2].childNodes[0].setAttribute("class","cell");
+	element.childNodes[2].childNodes[0].appendChild(document.createElement("strong"));
+	element.childNodes[2].childNodes[0].childNodes[0].innerText = "Mission Ids:"
+	element.childNodes[2].childNodes[0].childNodes[0].setAttribute("class","cell")
+	element.childNodes[2].appendChild(document.createElement("span"));
+	element.childNodes[2].childNodes[1].innerText="cfea"
+	
+	a.appendChild(document.createElement("div"))
+	a.appendChild(document.createElement("div"))
+	a.appendChild(document.createElement("div"))
+	document.body.getElementsByClassName("spaceX")
 }
